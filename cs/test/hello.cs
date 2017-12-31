df14d07133b9ae1239e1b3a73f730578b77592ff
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-12-31 10:50:15 +0800

    C# init

---
+++ cs/test/hello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using netlib;
+
+namespace Stack_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tcpmgr tmgr = new tcpmgr();
+            Console.WriteLine("hello");
+        }
+    }
+}
