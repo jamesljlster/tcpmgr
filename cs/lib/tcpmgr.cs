437cce20e545b58e3e671cb7155a6a34874dce80
Zheng-Ling Lai <jamesljlster@gmail.com>
2018-01-04 00:10:21 +0800

    Update types

--- cs/lib/tcpmgr.cs
+++ cs/lib/tcpmgr.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace netlib
 {
     public partial class tcpmgr
     {
+        // Private data types
+        private delegate void tcpmgr_client_task(object usrData);
+
+        private class tcpmgr_list
+        {
+            Thread tHandle;
+
+            int sockStatus;
+            Socket clientSock;
+
+            int occupied;
+            int closeJoin;
+
+            tcpmgr_client_task client_task;
+            object usrData;
+        }
+
         // Constructor
         public tcpmgr(String hostIP, int hostPort, int maxClient)
         {
@@ -21,6 +39,21 @@ namespace netlib
         }
 
         // Private objects
+        int stop;
+        int mgrListLen;
+        tcpmgr_list[] mgrList;
+
+        int serverFlag;
+
+        int cleanTaskStatus;
+        Thread cleanTask;
+
+        int acceptTaskStatus;
+        Thread acceptTask;
+
         Socket listenSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        tcpmgr_client_task client_task;
+        object usrData;
     }
 }
