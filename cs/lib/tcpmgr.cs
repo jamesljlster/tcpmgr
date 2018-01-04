b08d001054c1d294d0284eccd4881c1145b68784
Zheng-Ling Lai <jamesljlster@gmail.com>
2018-01-04 16:28:35 +0800

    Using TcpListener to finish tcpmgr module

--- cs/lib/tcpmgr.cs
+++ cs/lib/tcpmgr.cs
@@ -13,6 +13,7 @@ namespace netlib
         private class tcpmgr_list
         {
             public Thread tHandle;
+            public object cond;
 
             public int sockStatus;
             public Socket clientSock;
@@ -28,35 +29,32 @@ namespace netlib
         public tcpmgr(String hostIP, int hostPort, int maxClient)
         {
             // Check argument
-            if(maxClient <= 0)
+            if (maxClient <= 0)
             {
                 throw new ArgumentException("Invalid maxClient argument");
             }
 
             // Create client manage list
             this.mgrList = new tcpmgr_list[maxClient];
-            this.mgrListLen = maxClient;
 
             // Bind and listen
-            IPEndPoint hostAddrInfo = new IPEndPoint(IPAddress.Parse(hostIP), hostPort);
-            this.listenSock.Bind(hostAddrInfo);
-            this.listenSock.Listen(maxClient);
+            this.listenSock = new TcpListener(IPAddress.Parse(hostIP), hostPort);
         }
 
         // Destructor
         ~tcpmgr()
         {
-
+            this.listenSock.Stop();
+            this.stop();
         }
 
         // Private objects
-        int stop;
-        int mgrListLen;
+        int stopFlag;
         tcpmgr_list[] mgrList;
 
         int serverFlag;
 
-        object cond;
+        object cond = new object();
 
         int cleanTaskStatus;
         Thread cleanTask;
@@ -64,9 +62,65 @@ namespace netlib
         int acceptTaskStatus;
         Thread acceptTask;
 
-        Socket listenSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        TcpListener listenSock;
 
         tcpmgr_client_task client_task;
         object usrData;
+
+        public void start(tcpmgr_client_task client_task, object arg)
+        {
+            this.client_task = client_task;
+            this.usrData = arg;
+
+            // Create clean task
+            this.cleanTask = new Thread(tcpmgr_clean_task);
+            this.cleanTask.Start();
+
+            // Create accept task
+            this.acceptTask = new Thread(tcpmgr_accept_thread);
+            this.acceptTask.Start();
+        }
+
+        public void stop()
+        {
+            // Cancel and join accept task
+            if(this.acceptTask.IsAlive)
+            {
+                this.acceptTask.Abort();
+            }
+
+            // Cancel clean task
+            if(this.cleanTask.IsAlive)
+            {
+                this.cleanTask.Abort();
+            }
+
+            // Cancel all client task
+            this.server_cleanup();
+        }
+
+        private void server_cleanup()
+        {
+            for(int i = 0; i < this.mgrList.Length; i++)
+            {
+                if(this.mgrList[i].occupied > 0)
+                {
+                    this.mgrList[i].tHandle.Abort();
+                    this.mgrList[i].closeJoin = 1;
+                }
+
+                if(this.mgrList[i].closeJoin > 0)
+                {
+                    this.mgrList[i].tHandle.Join();
+                    this.mgrList[i].closeJoin = 0;
+                    this.mgrList[i].occupied = 0;
+                }
+
+                if(this.mgrList[i].clientSock.Connected)
+                {
+                    this.mgrList[i].clientSock.Close();
+                }
+            }
+        }
     }
 }
--- cs/lib/tcpmgr_thread.cs
+++ cs/lib/tcpmgr_thread.cs
@@ -7,7 +7,7 @@ namespace netlib
 {
     partial class tcpmgr
     {
-        private void tcpmgr_client_thread(ref object obj)
+        private void tcpmgr_client_thread(object obj)
         {
             tcpmgr_list listRef = (tcpmgr_list)obj;
 
@@ -19,17 +19,80 @@ namespace netlib
             listRef.sockStatus = 0;
 
             // Cleanup
-            Monitor.Pulse(this.cond);
+            Monitor.Pulse(listRef.cond);
             listRef.closeJoin = 1;
         }
 
-        private void tcpmgr_accept_thread(ref object obj)
+        private void tcpmgr_accept_thread(object obj)
         {
             tcpmgr mgrRef = (tcpmgr)obj;
 
-            while(mgrRef.stop == 0)
+            while (mgrRef.stopFlag == 0)
             {
-                Socket clientSocket = this.listenSock.Accept();
+                Console.WriteLine("Waiting for client connection...");
+                
+                while(mgrRef.stopFlag == 0 && !mgrRef.listenSock.Pending())
+                {
+                    Thread.Sleep(100);
+                }
+
+                // Accept client
+                Socket clientSock = mgrRef.listenSock.AcceptSocket();
+
+                // Lock client list
+                lock(mgrRef.mgrList)
+                {
+                    // Search empty entity
+                    int tmpIndex = -1;
+                    for(int i = 0; i < mgrRef.mgrList.Length; i++)
+                    {
+                        if(mgrRef.mgrList[i].occupied == 0)
+                        {
+                            tmpIndex = i;
+                            break;
+                        }
+                    }
+
+                    // Checking
+                    if(tmpIndex < 0)
+                    {
+                        Console.WriteLine("No more entity for client available. Connection rejected!");
+                        clientSock.Close();
+                    }
+                    else
+                    {
+                        // Set argument
+                        mgrRef.mgrList[tmpIndex].client_task = mgrRef.client_task;
+                        mgrRef.mgrList[tmpIndex].usrData = mgrRef.usrData;
+                        mgrRef.mgrList[tmpIndex].cond = mgrRef.cond;
+
+                        mgrRef.mgrList[tmpIndex].clientSock = clientSock;
+
+                        // Create client thread
+                        mgrRef.mgrList[tmpIndex].tHandle = new Thread(tcpmgr_client_thread);
+                        mgrRef.mgrList[tmpIndex].tHandle.Start((object)mgrRef.mgrList[tmpIndex]);
+                        mgrRef.mgrList[tmpIndex].occupied = 1;
+                    }
+                }
+            }
+        }
+
+        private void tcpmgr_clean_task(object obj)
+        {
+            tcpmgr mgrRef = (tcpmgr)obj;
+
+            // Wait condition
+            Monitor.Wait(mgrRef.cond);
+
+            // Join client thread
+            for(int i = 0; i < mgrRef.mgrList.Length; i++)
+            {
+                if(mgrRef.mgrList[i].closeJoin > 0)
+                {
+                    mgrRef.mgrList[i].tHandle.Join();
+                    mgrRef.mgrList[i].closeJoin = 0;
+                    mgrRef.mgrList[i].occupied = 0;
+                }
             }
         }
     }
